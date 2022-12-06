b8d1fe8ae197448c3e2157542b397cbe31ccad77
Junseong Park <junseongpark.dev@gmail.com>
2022-12-06 13:50:00 +0900

    feat: initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Archive</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: '#',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/UXev',
   footer: {
     text: 'Nextra Docs Template',
   },
-}
+};
 
-export default config
+export default config;
